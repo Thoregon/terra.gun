2c226d74542fa59cfa8bf843add218becb4925c1
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-01-03 21:04:04 +0100

    refactor GunService

--- index.mjs
+++ index.mjs
@@ -4,37 +4,6 @@
  * @author: Bernhard Lukassen
  */
 
-import { myterra }      from '/evolux.universe';
 import GunService       from "./lib/gunservice.mjs";
 
-
-export const service = {
-    install() {
-        console.log('** gun install()');
-        myterra().gun = new GunService();
-    },
-
-    uninstall() {
-        console.log('** gun uninstall()');
-        delete myterra().gun;
-    },
-
-    resolve() {
-        console.log('** gun resolve()');
-        // nothing to do
-    },
-
-    start() {
-        console.log('** gun start()');
-        myterra().gun.start();
-    },
-
-    stop() {
-        console.log('** gun stop()');
-        // myterra().gun.exit();    // nothing to do; gun keeps syncing as long as the process is running
-    },
-
-    update() {
-        console.log('** matter update()');
-    }
-};
+export const service = new GunService();
