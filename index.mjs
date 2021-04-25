150b652e7128782abf233d936396b3e20f5f0e6f
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-04-25 21:45:03 +0200

    PULS: IPFS integration

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@
  * @author: Bernhard Lukassen
  */
 
-export { default as Gun }       from '/gun';
+export { default as Gun }       from './gun/gun.js';
 
 import GunService               from "./lib/gunservice.mjs";
 export const service =          new GunService();
